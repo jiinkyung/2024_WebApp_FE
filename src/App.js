63e68e64e8b80ee627d99d7636851e6ead216061
jiinkyung <jinkyung010310@gmail.com>
2024-04-08 16:15:11 +0900

    feat: 0408 수업코드

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import Todo from './Todo';
+import React, { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Todo/>
-    </div>
+  const[items, setItems] = useState([
+    {
+      id: "0",
+      title: "Hello World!",
+      done: true 
+    }, {
+      id: "1",
+      title: "Hello World!2",
+      done: false 
+    },{
+      id: "2",
+      title: "Hello World!3",
+      done: false 
+    }]
   );
+
+  // JSX 결과를 변수에 저장
+  let todoItems =
+    items.length > 0 &&
+    items.map((item) => <Todo item={item} key={item.id} />);
+
+  return(
+    <div className='App'>
+     {/* <Todo item={item[0]}/>
+      <Todo item={item[1]}/> */}
+      {todoItems}
+
+    </div>
+  )
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = () => {
-    return(
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
-            <label for="todo0">Todo 컴포넌트 만들기</label>
-        </div>
-    )
+const Todo = (props) => {
+
+    // 매개변수 -> (itme, setItem): 상태변수, 상태변수를 업데이트해주는 함수
+    const [item, setItem] = useState(props.item);
+
+    let output = <div className="Todo">
+        <input 
+            type="checkbox" 
+            id={item.id} 
+            name={item.id}
+            checked={item.done}
+        />
+        <label for={item.id}>{item.title} </label>
+    </div>
+
+    return output;
 }
 
 export default Todo;
